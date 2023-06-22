775cf6f3192d0a7fb7efd79499fb2c7f5af2b999
lucasramalho.s97 <lucasramalho.s97@gmail.com>
2023-06-21 23:15:43 -0300

    Teste para o método main

--- src/test/java/com/api/BicicletarioAluguelApplicationTests.java
+++ src/test/java/com/api/BicicletarioAluguelApplicationTests.java
@@ -1,13 +1,21 @@
 package com.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class BicicletarioAluguelApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	public void mainTest() {
+		assertDoesNotThrow(() -> {
+			SpringApplication application = new SpringApplication(BicicletarioAluguelApplication.class);
+			application.setWebApplicationType(WebApplicationType.NONE);
+			application.run();
+		});	}
 	
 }
