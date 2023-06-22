c0655b558c36505e8edef61a85289494937e20c1
lucasramalho.s97 <lucasramalho.s97@gmail.com>
2023-06-22 01:28:09 -0300

    Retirando anotação Data de Passaporte

--- src/main/java/com/api/vo/Passaporte.java
+++ src/main/java/com/api/vo/Passaporte.java
@@ -1,12 +1,14 @@
 package com.api.vo;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Passaporte {
