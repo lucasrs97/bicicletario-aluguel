123eb5ebc4a852972f6cdefa849aff6aa85bd240
lucasramalho.s97 <lucasramalho.s97@gmail.com>
2023-06-22 01:09:45 -0300

    Retirando anotação Data de CartaoDeCredito

--- src/main/java/com/api/model/CartaoDeCredito.java
+++ src/main/java/com/api/model/CartaoDeCredito.java
@@ -1,12 +1,11 @@
 package com.api.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Date;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartaoDeCredito {
